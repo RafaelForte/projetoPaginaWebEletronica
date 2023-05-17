dffbd1c11535a82d884bc1b4ef56d581561e40ba
titovsky <rafaelforterf@gmail.com>
2023-05-17 11:12:56 -0300

    Adicionei mais uma imagem ao slider do topo, ajustei o tamanho do mesmo.

--- js/main.js
+++ js/main.js
@@ -5,5 +5,73 @@ $('.slider-principal').slick({
     slidesToShow: 1,
     adaptiveHeight: true,
     autoplay: true,
-    autoplaySpeed: 2000
+    autoplaySpeed: 2000,
+    
 });
+
+  
+  
+
+/*
+.slider-principal {
+    height: 400px;
+    overflow: hidden;
+    position: relative;
+  }
+  
+  .slider-principal .slick-list {
+    height: 100%;
+  }
+  
+  .slider-principal .slick-slide {
+    float: none !important;
+    display: inline-block;
+    height: 100%;
+  }
+  
+  .slider-principal .slick-slide img {
+    display: block;
+    width: 100%;
+    height: auto;
+    margin: 0 auto;
+    max-height: 400px;
+  }
+  
+  .slider-principal .slick-prev, 
+  .slider-principal .slick-next {
+    display: none;
+  }
+  
+  .slider-principal .slick-dots {
+    position: absolute;
+    bottom: 20px;
+    width: 100%;
+    text-align: center;
+  }
+  
+  .slider-principal .slick-dots li {
+    display: inline-block;
+    margin: 0 5px;
+  }
+  
+  .slider-principal .slick-dots li button {
+    font-size: 0;
+    line-height: 0;
+    display: block;
+    width: 20px;
+    height: 20px;
+    padding: 5px;
+    cursor: pointer;
+    color: transparent;
+    border: 0;
+    outline: none;
+    background: #fff;
+    border-radius: 50%;
+    opacity: 0.5;
+    transition: all 0.3s ease;
+  }
+  
+  .slider-principal .slick-dots li.slick-active button {
+    opacity: 1;
+  }
+  */
